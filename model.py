19a21f8c71d34dcef8c363aad098a50a566d6fa8
Vikky-V <vivekraj.offcial@gmail.com>
2025-12-12 22:05:34 +0530

    implements train_step procedure

--- model.py
+++ model.py
@@ -31,4 +31,30 @@ class QTrainer:
         self.criterion = nn.MSELoss()        
     
     def train_step(self, state, action, reward, next_state, done):
-        pass
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+        
+        # Q values with current state
+        prediction = self.model(state)
+        target = prediction.clone()
+        for index in range(len(done)):
+            Q_new = reward[index]
+            if not done[index]:
+                Q_new = reward[index] + self.gamma * torch.max(self.model(next_state[index]))
+            
+            target[index][torch.argmax(action).item()] = Q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, prediction)
+        loss.backward()
+        self.optimizer.step()
+
+        # Q_new = reward + gamma * max(next predicted Q value)
